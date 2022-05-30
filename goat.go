b5f84abcca512d95d159b545d5ecaafd6e0025ac
Tamir Bahar <tamir.bahar@gmail.com>
2022-05-30 21:58:47 +0300

    Finishing Touches
    - Support for int flags
    - cleanup

--- goat.go
+++ goat.go
@@ -21,6 +21,7 @@ func Run(f any) {
 
 type Flag[T any] interface {
 	flag(T)
+	CliName(defaultName string) string
 }
 
 type BoolFlag struct {
@@ -30,44 +31,160 @@ type BoolFlag struct {
 }
 
 func (flag BoolFlag) flag(bool) {}
+func (flag BoolFlag) CliName(defaultName string) string {
+	if flag.Name == "" {
+		return defaultName
+	}
+	return flag.Name
+}
 
 func (flag BoolFlag) AsCliFlag(defaultName string, dest *bool) cli.Flag {
-	name := flag.Name
-	if name == "" {
-		name = defaultName
-	}
 	if flag.Default {
 		return &cli.BoolTFlag{
-			Name:        name,
+			Name:        flag.CliName(defaultName),
 			Usage:       flag.Usage,
 			Destination: dest,
 		}
 	} else {
 		return &cli.BoolFlag{
-			Name:        name,
+			Name:        flag.CliName(defaultName),
 			Usage:       flag.Usage,
 			Destination: dest,
 		}
 	}
 }
 
-type StringFlag struct {
+type RequiredStringFlag struct {
+	Name  string
+	Usage string
+}
+
+type StringFlag = RequiredStringFlag
+
+func (flag RequiredStringFlag) flag(string) {}
+func (flag RequiredStringFlag) CliName(defaultName string) string {
+	if flag.Name == "" {
+		return defaultName
+	}
+	return flag.Name
+}
+
+func (flag RequiredStringFlag) AsCliFlag(defaultName string, dest *string) cli.Flag {
+	return &cli.StringFlag{
+		Name:        flag.CliName(defaultName),
+		Usage:       flag.Usage,
+		Required:    true,
+		Destination: dest,
+	}
+}
+
+type DefaultStringFlag struct {
 	Name    string
 	Usage   string
 	Default string
 }
 
-func (flag StringFlag) flag(string) {}
-
-func (flag StringFlag) AsCliFlag(defaultName string, dest *string) cli.Flag {
-	name := flag.Name
-	if name == "" {
-		name = defaultName
+func (flag DefaultStringFlag) flag(string) {}
+func (flag DefaultStringFlag) CliName(defaultName string) string {
+	if flag.Name == "" {
+		return defaultName
 	}
+	return flag.Name
+}
+
+func (flag DefaultStringFlag) AsCliFlag(defaultName string, dest *string) cli.Flag {
 	return &cli.StringFlag{
-		Name:        name,
+		Name:        flag.CliName(defaultName),
 		Usage:       flag.Usage,
 		Value:       flag.Default,
+		Destination: dest,
+	}
+}
+
+type OptionalStringFlag struct {
+	Name  string
+	Usage string
+}
+
+func (flag OptionalStringFlag) flag(*string) {}
+func (flag OptionalStringFlag) CliName(defaultName string) string {
+	if flag.Name == "" {
+		return defaultName
+	}
+	return flag.Name
+}
+
+func (flag OptionalStringFlag) AsCliFlag(defaultName string, dest *string) cli.Flag {
+	return &cli.StringFlag{
+		Name:        flag.CliName(defaultName),
+		Usage:       flag.Usage,
+		Required:    false,
+		Destination: dest,
+	}
+}
+
+type RequiredIntFlag struct {
+	Name  string
+	Usage string
+}
+
+type IntFlag = RequiredStringFlag
+
+func (flag RequiredIntFlag) flag(int) {}
+func (flag RequiredIntFlag) CliName(defaultName string) string {
+	if flag.Name == "" {
+		return defaultName
+	}
+	return flag.Name
+}
+func (flag RequiredIntFlag) AsCliFlag(defaultName string, dest *int) cli.Flag {
+	return &cli.IntFlag{
+		Name:        flag.CliName(defaultName),
+		Usage:       flag.Usage,
+		Required:    true,
+		Destination: dest,
+	}
+}
+
+type DefaultIntFlag struct {
+	Name    string
+	Usage   string
+	Default int
+}
+
+func (flag DefaultIntFlag) flag(int) {}
+func (flag DefaultIntFlag) CliName(defaultName string) string {
+	if flag.Name == "" {
+		return defaultName
+	}
+	return flag.Name
+}
+func (flag DefaultIntFlag) AsCliFlag(defaultName string, dest *int) cli.Flag {
+	return &cli.IntFlag{
+		Name:        flag.CliName(defaultName),
+		Usage:       flag.Usage,
+		Value:       flag.Default,
+		Destination: dest,
+	}
+}
+
+type OptionalIntFlag struct {
+	Name  string
+	Usage string
+}
+
+func (flag OptionalIntFlag) flag(*int) {}
+func (flag OptionalIntFlag) CliName(defaultName string) string {
+	if flag.Name == "" {
+		return defaultName
+	}
+	return flag.Name
+}
+func (flag OptionalIntFlag) AsCliFlag(defaultName string, dest *int) cli.Flag {
+	return &cli.IntFlag{
+		Name:        flag.CliName(defaultName),
+		Usage:       flag.Usage,
+		Required:    false,
 		Destination: dest,
 	}
 }
@@ -77,3 +194,10 @@ type Descriptor[T any] struct{}
 func (d Descriptor[T]) As(Flag[T]) {}
 
 func Describe[T any](arg T) Descriptor[T] { return Descriptor[T]{} }
+
+func GetOptional[T any](c *cli.Context, name string, dest *T) *T {
+	if c.IsSet(name) {
+		return dest
+	}
+	return nil
+}
--- sample/main.go
+++ sample/main.go
@@ -5,17 +5,24 @@ import (
 	"github.com/tmr232/goat"
 )
 
-//go:generate go run github.com/tmr232/goat/cmd/goat
+//go:generate go run github.com/tmr232/goat/cmd/goater
 
-func app(name string, goodbye bool) {
-	goat.Describe(name).As(goat.StringFlag{Usage: "The name to greet"})
-	goat.Describe(goodbye).As(goat.BoolFlag{Usage: "Enable to say Goodbye"})
+func app(name string, goodbye bool, question *string, times int) {
+	goat.Describe(name).As(goat.RequiredStringFlag{Usage: "The name to greet"})
+	goat.Describe(goodbye).As(goat.BoolFlag{Usage: "Enable to say Goodbye", Name: "bye"})
+	goat.Describe(question).As(goat.OptionalStringFlag{Usage: "Instead of a greeting, ask a question."})
+	goat.Describe(times).As(goat.DefaultIntFlag{Usage: "Number of repetitions", Default: 1})
 
-	if goodbye {
-		fmt.Printf("Goodbye, %s.\n", name)
-	} else {
-		fmt.Printf("Hello, %s!\n", name)
-
+	for i := 0; i < times; i++ {
+		if question != nil {
+			fmt.Printf("%s, %s?", *question, name)
+		} else {
+			if goodbye {
+				fmt.Printf("Goodbye, %s.\n", name)
+			} else {
+				fmt.Printf("Hello, %s!\n", name)
+			}
+		}
 	}
 }
 
