571787b74e257cae9eafb4d9794fbf07510a4aef
Tamir Bahar <tamir.bahar@gmail.com>
2022-06-02 16:11:38 +0300

    Basic extra description language

--- goat.go
+++ goat.go
@@ -1,6 +1,7 @@
 package goat
 
 import (
+	"github.com/urfave/cli"
 	"reflect"
 )
 
@@ -17,3 +18,55 @@ func Register(app any, wrapper func()) {
 func Run(f any) {
 	registry[reflect.ValueOf(f)]()
 }
+
+type Flag[T any] interface {
+	flag(T)
+}
+
+type BoolFlag struct {
+	Name     string
+	Usage    string
+	Required bool
+	Value    bool
+}
+
+func (flag BoolFlag) flag(bool) {}
+
+func (flag BoolFlag) AsCliFlag(dest *bool) cli.Flag {
+	if flag.Value {
+		return &cli.BoolTFlag{
+			Name:        flag.Name,
+			Usage:       flag.Name,
+			Required:    flag.Required,
+			Destination: dest,
+		}
+	} else {
+		return &cli.BoolFlag{
+			Name:        flag.Name,
+			Usage:       flag.Name,
+			Required:    flag.Required,
+			Destination: dest,
+		}
+	}
+}
+
+type StringFlag struct {
+	Name     string
+	Usage    string
+	Required bool
+	Value    string
+}
+
+func (flag StringFlag) flag(string) {}
+
+func (flag StringFlag) AsCliFlag(dest *string) cli.Flag {
+	return &cli.StringFlag{
+		Name:        flag.Name,
+		Usage:       flag.Usage,
+		Required:    flag.Required,
+		Value:       flag.Value,
+		Destination: dest,
+	}
+}
+
+func Describe[T any](arg T, flagDef Flag[T]) {}
