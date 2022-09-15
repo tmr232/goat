dc947d925b248a9010d7b4efa852b91ce040d4d1
Tamir Bahar <tamir.bahar@gmail.com>
2022-09-15 14:58:47 +0300

    Simplify flag descriptions in goater

--- cmd/goater/fluent_test.go
+++ cmd/goater/fluent_test.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"go/ast"
 	"go/parser"
-	"go/token"
 	"testing"
 )
 
@@ -15,30 +14,4 @@ func Test_parseFluentChain(t *testing.T) {
 		t.Error(err)
 	}
 	fmt.Println(parseFluentChain(expr.(*ast.CallExpr)))
-}
-
-func Test_parseFluentDescription(t *testing.T) {
-	code := "goat.Flag(value).Usage(\"Hello, World!\").Default(1).Name(\"v\",\"value\")"
-
-	expr, err := parser.ParseExpr(code)
-	fset := token.NewFileSet()
-	if err != nil {
-		return
-	}
-	if err != nil {
-		t.Error(err)
-	}
-	chain := parseFluentChain(expr.(*ast.CallExpr))
-	description, err := parseFluentDescription(fset, chain, func(node ast.Expr) (string, error) { return "type", nil })
-	if err != nil {
-		t.Error(err)
-	}
-	fmt.Println(description)
-}
-
-func Test_structGen(t *testing.T) {
-	fmt.Printf("%#v\n", struct {
-		name string
-		Name string
-	}{"a", "b"})
 }
