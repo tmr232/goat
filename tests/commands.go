795a13e2b9f7b90a3da74b6da23bf3ef8e72fd33
Tamir Bahar <tamir.bahar@gmail.com>
2022-10-25 22:48:58 +0300

    Slightly cleaner tests

---
+++ tests/commands.go
@@ -0,0 +1,36 @@
+package tests
+
+import (
+	"github.com/tmr232/goat"
+)
+
+//go:generate go run github.com/tmr232/goat/cmd/goater
+
+func noFlags()         {}
+func intFlag(flag int) {}
+func renamedFlag(bla int) {
+	goat.Flag(bla).Name("flag")
+}
+
+// Documented has some neat docs!
+//
+// It's just so nice to document your code.
+func Documented() {}
+
+func flagUsage(num int, str string) {
+	goat.Flag(num).Usage("A number of things.")
+	goat.Flag(str).Usage("A piece of text.")
+}
+
+func defaultValue(num int) {
+	goat.Flag(num).Default(5)
+}
+
+func Register() {
+	goat.Command(noFlags)
+	goat.Command(intFlag)
+	goat.Command(renamedFlag)
+	goat.Command(Documented)
+	goat.Command(flagUsage)
+	goat.Command(defaultValue)
+}
