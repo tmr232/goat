ef23bca1e02f0b2b1b89469854e418785f5c9333
Tamir Bahar <tamir.bahar@gmail.com>
2023-01-30 23:34:38 +0200

    Adding support for hex numbers is easier than I expected!!!

--- sample/custom/main.go
+++ sample/custom/main.go
@@ -6,6 +6,8 @@ import (
 	"github.com/tmr232/goat/flags"
 	"github.com/urfave/cli/v2"
 	"go/types"
+	"strconv"
+	"strings"
 )
 
 //go:generate go run github.com/tmr232/goat/cmd/goater
@@ -28,7 +30,46 @@ func (h *customTypeHandler) GetFlag(c *cli.Context, name string) any {
 	return h.get(c, name)
 }
 
+type hexNumber uint64
+
+func (h *hexNumber) Set(value string) error {
+	num, err := strconv.ParseUint(strings.TrimPrefix(strings.ToLower(value), "0x"), 16, 32)
+	*h = hexNumber(num)
+	return err
+}
+
+func (h *hexNumber) String() string {
+	return fmt.Sprintf("0x%X", *h)
+}
+
+func withHex(hex hexNumber) {
+	goat.Flag(hex).Default(hexNumber(0x12))
+	fmt.Println(hex)
+}
+
 func init() {
+	flags.RegisterTypeHandler[hexNumber](&customTypeHandler{
+		make: func(name, usage string, defaultValue any) cli.Flag {
+			if defaultValue == nil {
+				val := hexNumber(0)
+				return &cli.GenericFlag{
+					Name:     name,
+					Usage:    usage,
+					Value:    &val,
+					Required: true,
+				}
+			}
+			val := defaultValue.(hexNumber)
+			return &cli.GenericFlag{
+				Name:  name,
+				Usage: usage,
+				Value: &val,
+			}
+		},
+		get: func(c *cli.Context, name string) any {
+			return *c.Generic(name).(*hexNumber)
+		},
+	})
 	flags.RegisterTypeHandler[customType](&customTypeHandler{
 		make: func(name, usage string, defaultValue any) cli.Flag {
 			if defaultValue == nil {
@@ -74,5 +115,6 @@ func f() {
 }
 
 func main() {
-	goat.Run(withCustomType)
+	goat.Run(withHex)
+	//goat.Run(withCustomType)
 }
