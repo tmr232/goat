9586b39a927bf0a8cfd6d821737bfa6e093bfa86
Tamir Bahar <tamir.bahar@gmail.com>
2023-02-01 23:07:35 +0200

    Simplified method for registering generic types.
    Still a mess,though

--- go.mod
+++ go.mod
@@ -3,13 +3,13 @@ module github.com/tmr232/goat
 go 1.18
 
 require (
+	github.com/approvals/go-approval-tests v0.0.0-20220530063708-32d5677069bd
 	github.com/pkg/errors v0.9.1
-	github.com/urfave/cli/v2 v2.19.2
+	github.com/urfave/cli/v2 v2.24.2
 	golang.org/x/tools v0.1.12
 )
 
 require (
-	github.com/approvals/go-approval-tests v0.0.0-20220530063708-32d5677069bd // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
--- sample/custom/main.go
+++ sample/custom/main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"fmt"
+	"go/types"
+	"reflect"
+	"strconv"
+	"strings"
+
 	"github.com/tmr232/goat"
 	"github.com/tmr232/goat/flags"
 	"github.com/urfave/cli/v2"
-	"go/types"
-	"strconv"
-	"strings"
 )
 
 //go:generate go run github.com/tmr232/goat/cmd/goater
@@ -42,16 +44,155 @@ func (h *hexNumber) String() string {
 	return fmt.Sprintf("0x%X", *h)
 }
 
-func withHex(hex hexNumber) {
-	goat.Flag(hex).Default(hexNumber(0x12))
-	fmt.Println(hex)
+func (h *hexNumber) FromString(value string) (any, error) {
+	num, err := strconv.ParseUint(strings.TrimPrefix(strings.ToLower(value), "0x"), 16, 32)
+	return hexNumber(num), err
 }
 
-func init() {
-	flags.RegisterTypeHandler[hexNumber](&customTypeHandler{
+func withHex(hex hexNumber, phex *hexNumber) {
+	goat.Flag(hex).Default(hexNumber(0x12))
+	fmt.Println(hex, phex)
+}
+
+type Custom interface {
+	String() string
+	FromString(value string) (any, error)
+}
+
+type CustomGeneric[T Custom] struct {
+	value T
+}
+
+func (c *CustomGeneric[T]) Set(value string) error {
+	v, err := c.value.FromString(value)
+	c.value = v.(T)
+	return err
+}
+
+func (c *CustomGeneric[T]) String() string {
+	return c.value.String()
+}
+
+type MyGeneric[T any] struct {
+	val        T
+	toString   func(*T) string
+	fromString func(*T, string) error
+}
+
+func (m *MyGeneric[T]) Set(value string) error {
+	return m.fromString(&m.val, value)
+}
+
+func (m *MyGeneric[T]) String() string {
+	return m.toString(&m.val)
+}
+
+func registerGen[T any]() {
+	genericType := reflect.TypeOf(*new(T))
+
+	flags.RegisterTypeHandler[T](&customTypeHandler{
+		make: func(name, usage string, defaultValue any) cli.Flag {
+			genericValue := reflect.New(genericType)
+			genericInterface := genericValue
+
+			if defaultValue == nil {
+				return &cli.GenericFlag{
+					Name:     name,
+					Usage:    usage,
+					Value:    genericInterface.Interface().(cli.Generic),
+					Required: true,
+				}
+			}
+
+			genericInterface = reflect.New(genericType)
+			genericInterface.Elem().Set(reflect.ValueOf(defaultValue))
+
+			return &cli.GenericFlag{
+				Name:  name,
+				Usage: usage,
+				Value: genericInterface.Interface().(cli.Generic),
+			}
+		},
+		get: func(c *cli.Context, name string) any {
+			return reflect.ValueOf(c.Generic(name)).Elem().Interface()
+		},
+	})
+}
+
+func registerGenOpt[T any]() {
+	genericType := reflect.TypeOf(*new(T))
+
+	flags.RegisterTypeHandler[*T](&customTypeHandler{
+		make: func(name, usage string, defaultValue any) cli.Flag {
+			genericValue := reflect.New(genericType)
+			genericInterface := genericValue
+
+			if defaultValue == nil {
+				return &cli.GenericFlag{
+					Name:  name,
+					Usage: usage,
+					Value: genericInterface.Interface().(cli.Generic),
+				}
+			}
+
+			genericInterface = reflect.New(genericType)
+			genericInterface.Elem().Set(reflect.ValueOf(defaultValue))
+
+			return &cli.GenericFlag{
+				Name:  name,
+				Usage: usage,
+				Value: genericInterface.Interface().(cli.Generic),
+			}
+		},
+		get: func(c *cli.Context, name string) any {
+			if c.IsSet(name) {
+				return reflect.ValueOf(c.Generic(name)).Interface()
+			}
+			return nil
+		},
+	})
+}
+
+func registerGeneric[T any](toString func(*T) string, fromString func(*T, string) error) {
+	flags.RegisterTypeHandler[T](&customTypeHandler{
+		make: func(name, usage string, defaultValue any) cli.Flag {
+			val := MyGeneric[T]{
+				toString:   toString,
+				fromString: fromString,
+			}
+			if defaultValue == nil {
+				return &cli.GenericFlag{
+					Name:     name,
+					Usage:    usage,
+					Value:    &val,
+					Required: true,
+				}
+			}
+			val.val = defaultValue.(T)
+			return &cli.GenericFlag{
+				Name:  name,
+				Usage: usage,
+				Value: &val,
+			}
+		},
+		get: func(c *cli.Context, name string) any {
+			return c.Generic(name).(*MyGeneric[T]).val
+		},
+	})
+}
+
+//func registerGeneric[T cli.Generic]() {
+//	genericType := reflect.TypeOf(*new(T))
+//	if genericType.Kind() == reflect.Pointer {
+//		value := reflect.
+//	}
+//}
+
+func registerCustom[As any, T Custom]() {
+	flags.RegisterTypeHandler[As](&customTypeHandler{
 		make: func(name, usage string, defaultValue any) cli.Flag {
 			if defaultValue == nil {
-				val := hexNumber(0)
+				var val CustomGeneric[T]
 				return &cli.GenericFlag{
 					Name:     name,
 					Usage:    usage,
@@ -67,9 +208,47 @@ func init() {
 			}
 		},
 		get: func(c *cli.Context, name string) any {
-			return *c.Generic(name).(*hexNumber)
+			return *c.Generic(name).(*As)
 		},
 	})
+}
+
+func init() {
+	// registerCustom[hexNumber, *hexNumber]()
+	registerGen[hexNumber]()
+	registerGenOpt[hexNumber]()
+	//registerGeneric[hexNumber](
+	//	func(t *hexNumber) string {
+	//		return fmt.Sprintf("0x%X", *t)
+	//	},
+	//	func(h *hexNumber, s string) error {
+	//		num, err := strconv.ParseUint(strings.TrimPrefix(strings.ToLower(s), "0x"), 16, 32)
+	//		*h = hexNumber(num)
+	//		return err
+	//	},
+	//)
+	//flags.RegisterTypeHandler[hexNumber](&customTypeHandler{
+	//	make: func(name, usage string, defaultValue any) cli.Flag {
+	//		if defaultValue == nil {
+	//			val := hexNumber(0)
+	//			return &cli.GenericFlag{
+	//				Name:     name,
+	//				Usage:    usage,
+	//				Value:    &val,
+	//				Required: true,
+	//			}
+	//		}
+	//		val := defaultValue.(hexNumber)
+	//		return &cli.GenericFlag{
+	//			Name:  name,
+	//			Usage: usage,
+	//			Value: &val,
+	//		}
+	//	},
+	//	get: func(c *cli.Context, name string) any {
+	//		return *c.Generic(name).(*hexNumber)
+	//	},
+	//})
 	flags.RegisterTypeHandler[customType](&customTypeHandler{
 		make: func(name, usage string, defaultValue any) cli.Flag {
 			if defaultValue == nil {
@@ -112,5 +291,5 @@ func init() {
 
 func main() {
 	goat.Run(withHex)
-	//goat.Run(withCustomType)
+	// goat.Run(withCustomType)
 }
